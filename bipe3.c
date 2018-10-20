64e9206b552f8af94ecf9e71617c5d504e154b80
heo2 <sgksals1@naver.com>
2018-10-21 00:20:30 +0900

    파이프3

--- bipe3.c
+++ bipe3.c
@@ -6,22 +6,24 @@
 
 int main(void) {
 	int pfd[2];
+	//파이프 생성
 	if(pipe(pfd) == -1){
 		perror("pipe error");
 		exit(1);
 	}
-
+	//포크 실행
 	if(!fork()){
-		close(1);
-		dup(pfd[1]);
+		close(1); // 표준 출력 (모니터를 닫음)
+		dup(pfd[1]);//표준 출력을 파이프의 입력으로 연결
 		close(pfd[0]);
-
+		//화면에 출력되어야 할 결과가 파이프의 입력으로 됨
 		execlp("ls","ls",NULL);
 	}
 	else {
-		close(0);
-		dup(pfd[0]);
+		close(0); //표준 입력(키보드 입력)을 닫음
+		dup(pfd[0]);//표준 입력을 파이프의 출력으로 연결
 		close(pfd[1]);
+		//파이프의 출력(즉, 자식 프로세스에서 실행 한 ls의 결과)이 wc의 입력이 됨
 		execlp("wc","wc","-l",NULL);
 	}
 	return 0;
