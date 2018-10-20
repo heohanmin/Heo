ffeb2c93f534d5649406c80d3cad8c422fffc433
heo2 <sgksals1@naver.com>
2018-10-20 22:02:47 +0900

    파이프2

---
+++ bipe2.c
@@ -0,0 +1,36 @@
+/*파이프 사용*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+int main(void) {
+	int pfd[2];
+	char buf[30];
+
+	/*파이프 생성*/
+	if(pipe(pfd) == -1){
+		perror("pipe error");
+		exit(1);
+	}
+
+	if(!fork()){
+		printf("CHILD(PID : %d): Writing to the pipe\n",getpid());
+
+		write(pfd[1],"pipe!",5);
+		
+		exit(0);
+	}
+	else {
+		printf("PARENT(PID : %d): Reading from the pipe\n",getpid());
+
+		read(pfd[0],buf,5);
+		printf("PARENT(PID : %d): %s\n",getpid(),buf);
+		wait(NULL);
+	}
+	return 0;
+}
+
